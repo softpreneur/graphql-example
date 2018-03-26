44a0be8da929e5b4ca59fb2434b93d75e5b2a9af
Ibrahim Abdulrahman <Motionwares@APPLEs-MacBook-Pro.local>
2018-03-26 08:50:16 +0100

    m

--- build/app.js
+++ build/app.js
@@ -1,5 +1,6 @@
 "use strict";
-
+require("babel-core/register");
+require("babel-polyfill");
 var _compression = require("compression");
 
 var _compression2 = _interopRequireDefault(_compression);
@@ -37,9 +38,6 @@ var _middlewares = require("./config/middlewares");
 var _middlewares2 = _interopRequireDefault(_middlewares);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-require("babel-core/register");
-require("babel-polyfill");
 
 var app = (0, _express2.default)();
 app.use((0, _compression2.default)());
