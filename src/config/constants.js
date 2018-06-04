188145d98e366d3ead35b415466d6a60d1027aa0
Ibrahim Abdulrahman Abdullahi <motionboy4@gmail.com>
2018-06-05 00:09:59 +0100

    Updating db connection url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,8 @@
 export default {
   PORT: process.env.PORT || 8080,
-  DB_URL: "mongodb://localhost/my_db",
+  DB_URL: "mongodb://root:Motionb2k@35.198.109.62:27017/admin",
+  //DB_URL: "mongodb://localhost:27017/vp_db",
   GRAPHQL_PATH: "/graphql",
-  JWT_SECRET: "your-own-secret",
+  JWT_SECRET: "motionboyforlife@cahona",
   SUBSCRIPTIONS_PATH: "/subscriptions"
 };
